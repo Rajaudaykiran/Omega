905da33661c876fecc8e8c07084ad604bda1e019
Rajaudaykiran <69520421+Rajaudaykiran@users.noreply.github.com>
2020-08-11 20:15:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAdP4-P_SdlCvouMH-ELLU0i14j7RcGEX8",
+  authDomain: "facebook-6d1ad.firebaseapp.com",
+  databaseURL: "https://facebook-6d1ad.firebaseio.com",
+  projectId: "facebook-6d1ad",
+  storageBucket: "facebook-6d1ad.appspot.com",
+  messagingSenderId: "34352636606",
+  appId: "1:34352636606:web:349c2cff3e3e7d382774ef",
+  measurementId: "G-4J3RMKXSE4"
 };
-
      
  
 ////Do Not Edit Below This
